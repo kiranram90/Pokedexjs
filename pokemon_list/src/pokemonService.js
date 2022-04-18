973e24854d74003c011d4bf05a36691754bf42a9
Kiran Ram <keyrun@Kirans-MacBook-Pro.local>
2022-04-18 00:50:55 -0400

    fetch endpoint fixed

--- pokemon_list/src/pokemonService.js
+++ pokemon_list/src/pokemonService.js
@@ -18,7 +18,23 @@ class PokemonService{               //have a base url. so you don't have to worr
         })
     }
 
-    
+    createDetail(){
+        const detail = { 
+            name: document.getElementById('name').value, 
+            elements: document.getElementById('elements').value,
+            size: document.getElementById('size').value,
+            location: document.getElementById('location').value,
+            //pokemon id needs to be set
+        }
+        const configObject = {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(detail) //everything shared on the internet is string so we need to stringfy it
+        }
+
+      
         })
 
     }    
